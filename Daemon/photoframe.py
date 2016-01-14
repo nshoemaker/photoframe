7c5735d9c6b352a06ef6d0f7a9f8d2f416cc9e0e
Clayton Ritcher <claytonritcher@gmail.com>
2016-01-14 16:37:56 -0500

    Updated the main app to take in a location to use

--- Daemon/photoframe.py
+++ Daemon/photoframe.py
@@ -5,27 +5,32 @@ import logging.handlers
 import argparse
 import sys
 
-sys.path.append('/home/nora/Nora/Build18/photoframe/Email')
-from AttachmentDownloader import AttachmentDownloader
-from Credentials import Credentials
-sys.path.append('/home/nora/Nora/Build18/photoframe/')
-from slideshow import App
-
-
-# Deafults
-LOG_FILENAME = "/tmp/photoframe.log"
-LOG_LEVEL = logging.INFO  # Could be e.g. "DEBUG" or "WARNING"
-
-PIC_LOCATION = "/home/nora/Nora/attachments"
+LOG_FILENAME = "/usr/local/log/photoframe.log"
 
 # Define and parse command line arguments
 parser = argparse.ArgumentParser(description="Service for photoframe")
 parser.add_argument("-l", "--log", help="file to write log to (default '" + LOG_FILENAME + "')")
+parser.add_argument('installDir', metavar='D', help='The directory where the python files are')
 
 # If the log file is specified on the command line then override the default
 args = parser.parse_args()
 if args.log:
         LOG_FILENAME = args.log
+
+BASE_DIR = args.installDir + "/"
+
+# Deafults
+LOG_FILENAME = BASE_DIR + "photoframe.log"
+LOG_LEVEL = logging.INFO  # Could be e.g. "DEBUG" or "WARNING"
+
+
+PIC_LOCATION = BASE_DIR + "attachments"
+
+sys.path.append(BASE_DIR + 'Email')
+from AttachmentDownloader import AttachmentDownloader
+from Credentials import Credentials
+sys.path.append(BASE_DIR)
+from slideshow import App
 
 # Configure logging to log to a file, making a new file at midnight and keeping the last 3 day's data
 # Give the logger a unique name (good practice)
